0d10b3d35fa2acfd18fe454339771f8c03f5009c
Amitojasa <singh99amitoj@gmail.com>
2021-07-14 19:20:49 +0530

    todo app in react and material ui

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+
+
+
+import { TextField, List, Button, ListItem, ListItemText, ListItemSecondaryAction, Checkbox } from '@material-ui/core'
+import './App.css'
+const App = () => {
+
+  const [list, setList] = useState([])
+
+  const [val, setVal] = useState("")
+
+
+  const handleChange = (event) => {
+
+    event.preventDefault();
+    // var a = [];
+
+
+    // list.map(item => {
+    //   a.push(item)
+    // })
+    // a.push(val)
+    // setList(a)
+
+    if (val === "") {
+
+    } else {
+      setList([...list, val])
+
+
+      setVal("");
+    }
+
+
+  }
+
+  return (
+    <div className='App'>
+      <h1>Todo</h1>
+      <h2> SSDC</h2>
+      <form >
+        {/* <div > */}
+        <TextField id="standard-basic" label="To-Do" type="text" value={val} onChange={event => setVal(event.target.value)} />
+
+        <Button variant="contained" color="primary" type="submit" onClick={(event) => handleChange(event)}>Add</Button>
+        {/* </div> */}
+      </form>
+      {/* <List dense className={classes.root}> */}
+      <List dense className="list" >
+        {
+          list.map((item, index) => {
+
+            return (
+              <ListItem key={index} button>
+                {/* <ListItemAvatar>
+                <Avatar
+                  alt={`Avatar n°${value + 1}`}
+                  src={`/static/images/avatar/${value + 1}.jpg`}
+                />
+              </ListItemAvatar> */}
+                <ListItemText id={index} primary={item} />
+                <ListItemSecondaryAction>
+                  <Checkbox
+
+                  />
+                </ListItemSecondaryAction>
+              </ListItem>
+            )
+          })
+
+        }
+      </List>
+
+
+    </div >
+
+  )
+
+}
+
+
+export default App;
